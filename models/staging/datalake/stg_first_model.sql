f1859ab4b525037fde8713a9f53be5f0e85fc4f5
Suril Vara <suril.vara@launchmetrics.com>
2022-02-09 17:14:51 +0000

    added source yml file

--- models/staging/datalake/stg_first_model.sql
+++ models/staging/datalake/stg_first_model.sql
@@ -5,6 +5,6 @@ SELECT
     REPLACE(tag, "\"","") as tag_id,
     JSON_EXTRACT_SCALAR(tr, '$._id') as tagrel_id
 
-FROM `datalake-328011.test.media` as m, UNNEST(tags) as tag
+FROM {{ source('datalake', 'media') }} as m, UNNEST(tags) as tag
 LEFT JOIN UNNEST(m.tagRelationships) as tr
 WHERE _id IN ('61df14bf32be0e00098f36d3', '61df14bfe09f4b0009186f15', '61df17ecbb547f0009c0e72c')
